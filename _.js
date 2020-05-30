04021849b0a085f75d2746f14b5935639f28d5d9
etgreer91 <etgreer91@gmail.com>
2020-05-30 11:20:38 -1000

    adds implementation for clamp and inRange

--- _.js
+++ _.js
@@ -1,7 +1,26 @@
 
+const _ = {
+    clamp(number, lower, upper) {
+        let lowerClampedValue = Math.max(number, lower);
+        let clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+    },
 
+    inRange(number, start, end) {
+        if (end === undefined) {
+            end = start
+            start = 0
+        }
 
+        if (start > end) {
+            let temp = end
+            end = start
+            start = temp
+        }
 
+        return number >= start && number < end
+    },
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
